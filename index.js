0fe7a045237b559ddd5071bb5b266e15eeba519f
justin <justinmwathi32@gmail.com>
2024-04-05 22:30:51 +0300

    Lab completed

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  function hasTargetSum(array, target) {
+    // Iterate through every number starting from index[0]
+    for (let i = 0; i < array.length; i++) {
+      // Still iterate through each number but with an index ahead of i
+      for (let k = i + 1; k < array.length; k++) {
+        // If the number[i] + number[k] equals the target, return true
+        if (array[i] + array[k] === target) {
+          return true;
+        }
+      }
+    }
+    // If no pair of numbers adds up to the target, return false
+    return false;
+  }
 }
 
 /* 
   Write the Big O time complexity of your function here
+O(n*a)
+n=6;
+a=5;
+Therefore:O(6*5=30)
+O(30);
 */
 
 /* 
   Add your pseudocode here
 */
+//the function hasTargetSum(array,targetNumber)-it takes two arguments.
+//if the sum of any numbers in the array equals to target number,the function should return true else it should return false.
 
 /*
   Add written explanation of your solution here
+  The function  returns true if any pair of numbers in the array adds up to the target number
 */
 
 // You can run `node index.js` to view these console logs
